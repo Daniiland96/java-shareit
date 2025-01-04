0ab332d1ebf35a550ff52c1ab904da64c99bd6b7
Daniil Andreev <daniiland9@yandex.ru>
2025-01-05 00:56:33 +0300

    feat: add item requests and gateway (in progress)

--- server/src/main/java/ru/practicum/shareit/item/dto/CreateCommentDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/CreateCommentDto.java
@@ -1,9 +1,13 @@
 package ru.practicum.shareit.item.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
+@EqualsAndHashCode
 public class CreateCommentDto {
     private String text;
 }
--- server/src/main/java/ru/practicum/shareit/item/dto/UpdateItemRequest.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/UpdateItemRequest.java
@@ -1,10 +1,8 @@
 package ru.practicum.shareit.item.dto;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
 public class UpdateItemRequest {
     private String name;
     private String description;
